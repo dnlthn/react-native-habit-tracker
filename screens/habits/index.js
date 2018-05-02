5629057bede0839b015eaa7501fdba24c55b27f3
Daniel Thompson <dthompson844@gmail.com>
2018-05-02 04:39:36 -0500

    Created components for the habits and history screens.

---
+++ screens/habits/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import ScreenLayout from '../../layouts';
+
+const Habits = () => (
+  <ScreenLayout>
+    <Text>Habits Screen!</Text>
+  </ScreenLayout>
+);
+
+export default Habits;
