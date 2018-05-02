2b06c87b944106d644736d04d278c4491aa59553
Daniel Thompson <dthompson844@gmail.com>
2018-05-02 07:33:29 -0500

    Added unstated provider to the root component.

--- App.js
+++ App.js
@@ -1,9 +1,21 @@
-import { TabNavigator } from 'react-navigation';
+import React from 'react';
 
+import { TabNavigator } from 'react-navigation';
 import HabitScreen from './screens/habits';
 import HistoryScreen from './screens/history';
 
-export default TabNavigator({
+import { Provider } from 'unstated';
+// import CounterContainer from './containers/counter'
+
+const TabComponent = TabNavigator({
   Habits: { screen: HabitScreen },
   History: { screen: HistoryScreen }
 });
+
+const App = () => (
+  <Provider>
+    <TabComponent />
+  </Provider>
+);
+
+export default App;
