bf946ee4e4a2bc06c5d974f3edfed6c684f003b6
Daniel Thompson <dthompson844@gmail.com>
2018-05-03 01:31:21 -0500

    Implemented addHabit to pass the test

--- containers/habits/index.js
+++ containers/habits/index.js
@@ -1,8 +1,26 @@
 import { Container } from 'unstated';
 
 class HabitsContainer extends Container {
+  habitId = 0;
   state = {
     habits: [],
+  };
+
+  addHabit = ({ title, frequency, timesPerDay }) => {
+    const nextState = [
+      ...this.state.habits,
+      {
+        frequency,
+        id: this.habitId,
+        status: 'ACTIVE',
+        timesPerDay,
+        timesRemainingToday: timesPerDay,
+        title,
+      },
+    ];
+    this.habitId = this.habitId + 1;
+
+    this.setState({ habits: nextState });
   };
 }
 
