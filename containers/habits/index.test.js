472925da9a172ebe5d4ad9b19fee7b5eddb01ea8
Daniel Thompson <dthompson844@gmail.com>
2018-05-03 02:59:12 -0500

    Added a function to generate habits and a test to pass an initial value to the constructor.
    The goal of this is to clean up some of the callback hell around the
    defer() function. It should result in our tests looking marginally
    cleaner, but unfortunately, won’t completely eliminate defer() calls.

--- containers/habits/index.test.js
+++ containers/habits/index.test.js
@@ -2,6 +2,20 @@ import defer from 'tickedoff';
 
 import HabitsContainer from './index';
 
+const generateHabits = numberOfHabits => {
+  const baseHabit = {
+    frequency: 'DAILY',
+    status: 'ACTIVE',
+    timesPerDay: 1,
+    timesRemainingToday: 1,
+    title: 'First Habit!',
+  };
+
+  return [...Array(numberOfHabits).keys()].map(index => ({
+    ...baseHabit,
+    id: index,
+  }));
+};
 it('returns an empty array for the default value', () => {
   const habitsContainer = new HabitsContainer();
   const { habits } = habitsContainer.state;
@@ -9,6 +23,21 @@ it('returns an empty array for the default value', () => {
   expect(habits).toHaveLength(0);
   expect(habits).toEqual([]);
 });
+
+it('returns the correct initial value', () => {
+  const generatedHabits = generateHabits(2);
+  const habitsContainer = new HabitsContainer({ habits: generatedHabits });
+  const { habits } = habitsContainer.state;
+
+  expect(habits).toMatchObject(generatedHabits);
+});
+
+// it('initial value from prop is set correctly', () => {
+//   const counter = new HabitsContainer({
+//     initialCount: 3,
+//   });
+//   expect(counter.state.count).toEqual(3);
+// });
 
 it('adds a new habit with the addHabit function', done => {
   const habitsContainer = new HabitsContainer();
