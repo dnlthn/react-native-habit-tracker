3d7a54bb1d55d5b13247cf1701dea5154996048a
Daniel Thompson <dthompson844@gmail.com>
2018-05-03 01:29:44 -0500

    Swapped empty array checking to be second

--- containers/habits/index.test.js
+++ containers/habits/index.test.js
@@ -4,7 +4,7 @@ it('returns an empty array for the default value', () => {
   const habitsContainer = new HabitsContainer();
   const { habits } = habitsContainer.state;
 
-  expect(habits).toEqual([]);
   expect(habits).toHaveLength(0);
+  expect(habits).toEqual([]);
 });
 
