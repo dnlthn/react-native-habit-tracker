c5e24803633c5170c071b16a68a4ec10432bdf37
Daniel Thompson <dthompson844@gmail.com>
2018-05-07 04:10:58 -0500

    Refactored perform() to handle the new data structure and added getIterable() and updateActive()

--- containers/habits/index.js
+++ containers/habits/index.js
@@ -47,21 +47,75 @@ class HabitsContainer extends Container {
     this.setState({ habits: nextState });
   };
 
-    const habit = this.state.habits[id];
   perform = id => {
+    if (!this.state[FILTERS.ACTIVE][id]) return;
+
+    const habit = this.state[FILTERS.ACTIVE][id];
     const timesRemainingToday = habit.timesRemainingToday - 1;
 
-    const nextState = {
-      ...this.state.habits,
+    if (timesRemainingToday === 0) {
+      const { [String(id)]: _, ...nextActiveState } = this.state[
+        FILTERS.ACTIVE
+      ];
 
-      [id]: {
-        ...habit,
-        timesRemainingToday,
-        status: timesRemainingToday === 0 ? STATUS.COMPLETED : habit.status,
-      },
+      const habit_history = this.state[FILTERS.HISTORY][id];
+      const nextHistoryState = {
+        ...this.state[FILTERS.HISTORY],
+
+        [id]: {
+          ...habit_history,
+          timesCompleted: habit_history ? habit_history.timesCompleted + 1 : 1,
+        },
+      };
+
+      this.setState({
+        [FILTERS.ACTIVE]: nextActiveState,
+        [FILTERS.HISTORY]: nextHistoryState,
+      });
+    } else {
+      const nextState = {
+        ...this.state[FILTERS.ACTIVE],
+
+        [id]: {
+          ...habit,
+          timesRemainingToday,
+        },
+      };
+
+      this.setState({ [FILTERS.ACTIVE]: nextState });
+    }
+  };
+
+  getIterable = ({ filter }) => {
+    const filterProperties = id => {
+      switch (filter) {
+        case FILTERS.ACTIVE:
+          return {
+            timesRemainingToday: this.state[FILTERS.ACTIVE][id]
+              .timesRemainingToday,
+          };
+
+        case FILTERS.HISTORY:
+          return {
+            timesCompleted: this.state[FILTERS.HISTORY][id].timesCompleted,
+          };
+
+        default:
+          return {};
+      }
     };
 
-    this.setState({ habits: nextState });
+    return Object.keys(this.state[filter]).map(id => ({
+      ...this.state.habits[id],
+      ...filterProperties(id),
+      id,
+    }));
+  };
+
+  updateActive = () => {
+    const nextState = { ...this.state.habits };
+
+    this.setState({ active: nextState });
   };
 }
 
