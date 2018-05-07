9e2dbff5a7daac00e8e45e80742afc7cf2ede24e
Daniel Thompson <dthompson844@gmail.com>
2018-05-07 04:03:22 -0500

    Added a DAYS object

--- containers/habits/constants.js
+++ containers/habits/constants.js
@@ -3,6 +3,16 @@ export const FILTERS = {
   HISTORY: 'history',
 };
 
+export const DAYS = {
+  SUNDAY: 'sunday',
+  MONDAY: 'monday',
+  TUESDAY: 'tuesday',
+  WEDNESDAY: 'wednesday',
+  THURSDAY: 'thursday',
+  FRIDAY: 'friday',
+  SATURDAY: 'saturday',
+};
+
 export const STATUS = {
   ACTIVE: 'active',
   INACTIVE: 'inactive',
