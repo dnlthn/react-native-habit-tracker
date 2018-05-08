210490c62b4f6dd127fbf1690057d6a77de3daed
Daniel Thompson <dthompson844@gmail.com>
2018-05-08 14:56:58 -0500

    Cleaned up information imports

--- components/habit/information.js
+++ components/habit/information.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { View } from 'react-native';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 
 import { DAYS } from '../../data/constants';
 
