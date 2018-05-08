61dec4ccb290742bb1a01bc44266da493ff809fd
Daniel Thompson <dthompson844@gmail.com>
2018-05-08 09:16:55 -0500

    Created a render prop for handling habit creation state

---
+++ containers/create-habit/index.js
@@ -0,0 +1,51 @@
+import React, { Fragment, Component } from 'react';
+
+export default class CreateHabit extends Component {
+  state = {
+    title: '',
+    frequency: [],
+    timesPerDay: 1,
+  };
+
+  updateTitle = text => this.setState({ title: text });
+
+  updateTimesPerDay = value => this.setState({ timesPerDay: value });
+
+  toggleDay = day => {
+    const nextFrequency = state =>
+      state.includes(day)
+        ? [
+            ...state.slice(0, state.indexOf(day)),
+            state[state.indexOf(day)] + 1,
+            ...state.slice(state.indexOf(day) + 1),
+          ]
+        : [...state, day];
+
+    this.setState(state => ({ frequency: nextFrequency(state.frequency) }));
+  };
+
+  reset = () => {
+    this.setState({
+      title: '',
+      frequency: [],
+      timesPerDay: 1,
+    });
+  };
+
+  render() {
+    return (
+      <Fragment>
+        {this.props.children({
+          addHabit: this.addHabit,
+          frequency: this.state.frequency,
+          reset: this.reset,
+          timesPerDay: this.state.timesPerDay,
+          title: this.state.title,
+          toggleDay: this.toggleDay,
+          updateTimesPerDay: this.updateTimesPerDay,
+          updateTitle: this.updateTitle,
+        })}
+      </Fragment>
+    );
+  }
+}
