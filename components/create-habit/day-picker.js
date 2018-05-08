37b674fc71e30cdb2a7f283eeba0cb660f3cc6be
Daniel Thompson <dthompson844@gmail.com>
2018-05-08 10:13:22 -0500

    Renamed the styled components to remove redudancy

--- components/create-habit/day-picker.js
+++ components/create-habit/day-picker.js
@@ -5,34 +5,34 @@ import styled from 'styled-components';
 import { DAYS } from '../../data/constants';
 
 const DayPicker = ({ frequency, toggleDay }) => (
-  <DaysWrapper>
+  <Wrapper>
     {Object.values(DAYS).map(day => (
       <TouchableOpacity key={day} onPress={() => toggleDay(day)}>
-        <DayButton selected={frequency.includes(day)}>
-          <DayText>{day.toUpperCase()}</DayText>
-        </DayButton>
+        <Button selected={frequency.includes(day)}>
+          <Day>{day.toUpperCase()}</Day>
+        </Button>
       </TouchableOpacity>
     ))}
-  </DaysWrapper>
+  </Wrapper>
 );
 
 export default DayPicker;
 
-const DaysWrapper = styled.View`
+const Wrapper = styled.View`
   flex-direction: row;
   flex-wrap: wrap;
 `;
 
-const DayButton = styled.View`
-  padding-horizontal: 10px;
-  padding-vertical: 10px;
+const Button = styled.View`
   align-items: center;
   background-color: #f0f1f4;
-  margin: 5px;
-  border-radius: 5px;
   background-color: ${({ selected }) => (selected ? '#fed780' : '#f0f1f4')};
+  border-radius: 5px;
+  margin: 5px;
+  padding-horizontal: 10px;
+  padding-vertical: 10px;
 `;
-const DayText = styled.Text`
+const Day = styled.Text`
   color: ${({ selected }) => (selected ? '#37394c' : '#0b0b0f')};
   font-size: 14px;
   letter-spacing: 0.75;
